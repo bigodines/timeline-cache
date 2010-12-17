cbc2b2621743df04943f4d0979fd3a8f7857bc85
matheus mendes <bigodines@yahoo.com.br>
2010-12-17 03:29:28 -0200

    working on multi_get at 3:30am ...

--- src/storage.py
+++ src/storage.py
@@ -3,41 +3,31 @@ import simplejson as json
 
 from server import TimelineItem
 
-def decode_item(dct):
-	if "__TimelineItem__" in dct:
-		ret = TimelineItem()
-		ret.id = dct['id']
-		ret.author = dct['author']
-		ret.timestamp = dct['timestamp']
-		return ret
-	return dct
-
-def encode_item(item):
-	if isinstance(item, TimelineItem):
-		return {'__TimelineItem__': True,
-				'id': item.id,
-				'timestamp': item.timestamp,
-				'author': item.author}
-	raise TypeError("You must serialize a valid TimelineItem object")
-
-
 class RedisStorage():
+	LIST_SIZE = 9
 	def __init__(self, client_instance=None):
 		self.storage = client_instance
 
 	def push(self, key, obj):
-		obj = json.dumps(obj, default=encode_item)
+		obj = json.dumps(obj)
 		pos = self.storage.lpush(key, obj)
 		# cant let this list get too big
-		self.storage.ltrim(key, 0, 9) 
+		self.storage.ltrim(key, 0, self.LIST_SIZE) 
 		return pos
 	
 	def get(self, key):
-		serialized =  self.storage.lpop(key)
-		return json.loads(serialized, object_hook=decode_item)
+		serialized =  self.storage.lrange(key, 0, self.LIST_SIZE)
+		return [json.loads(x) for x in serialized]
+
 
 	def multi_get(self, keys):
-		pass
+		pipe = self.storage.pipeline()
+		for k in keys:
+			pipe.lrange(k, 0, self.LIST_SIZE)
+
+		serialized = pipe.execute()
+		return serialized #[json.loads(x) for x in serialized]
+		
 
 
 
--- src/test_storage.py
+++ src/test_storage.py
@@ -1,7 +1,6 @@
 import redis
 import unittest
 
-from server import TimelineItem as Item
 from storage import *
 
 class RedisStorageTest(unittest.TestCase):
@@ -14,29 +13,45 @@ class RedisStorageTest(unittest.TestCase):
 		self.redis_obj.flushdb()
 		for c in self.redis_obj.connection_pool.get_all_connections():
 			c.disconnect()
-
 		self.storage = None
 
-
 	def test_single_push(self):
-		item = Item('foo', 'author')
+		item = { 'key': 'foo', 'value': 'bar' }
 		x = self.storage.push('foo', item)
 		self.assertEquals(1, x)
 
 	def test_single_push_and_get(self):
-		item = Item('foo', 'author')
+		item = { 'key': 'foo', 'value': 'bar' }
 		self.storage.push('foo', item)
-		x =self.storage.get('foo')
-		self.assertEquals(item.id, x.id)
-		self.assertEquals(item.timestamp, x.timestamp)
+		x = self.storage.get('foo')
+		self.assertEquals(item['key'], x[0]['key'])
+		self.assertEquals(item['value'], x[0]['value'])
 
 	def test_many_pushes_one_get(self):
-		one = Item('itemId', 'foobar')
+		one = { 'key': 'itemId', 'value': 'foobar' }
 		self.storage.push('my_items', one)
-		two = Item('itemId2', 'foobar2')
+		two = { 'key': 'itemId2', 'value': 'baz' }
 		self.storage.push('my_items',two)
 		resp = self.storage.get('my_items')
-		
+
+	def test_list_cant_get_too_big(self):
+		for x in range(20):
+			self.storage.push('some_key', x)
+
+		resp = self.storage.get('some_key')
+		self.assertEquals(10, len(resp))
+
+	def test_multi_get(self):
+		v1 = (123, 456)
+		v2 = (444, 333)
+		v3 = (555, 111)
+		self.storage.push('a', v1)
+		self.storage.push('b', v2)
+		self.storage.push('a', v3)
+
+		res = self.storage.multi_get(['a','b'])
+		self.assertEquals(3, len(res))
+	
 		
 		
 
